ac8e7d672e6ad08e461bc11533e4201af207e665
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2020-11-11 17:00:38 +0200

    Log errors

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const validate = require('./validate');
 // most @actions toolkit packages have async methods
 async function run() {
   const openapiFile = core.getInput('openapi-file');
+  core.info(`Validating file ${openapiFile}`);
   const errors = await validate(openapiFile);
   if (errors !== undefined) {
+    core.error(errors);
     core.setFailed(errors.message);
   }
 }
